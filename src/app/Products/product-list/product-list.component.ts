3181a56bd42aa27ab8e830ecc8d643b63d7d9957
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-04 21:49:53 +1000

    Added new component for product list

---
+++ src/app/Products/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  pageTitle: string = 'Product List';
+  products: any[];
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
