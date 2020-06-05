b59eb2095de3d5c90ca97b8fa62ddfa345ab6d9f
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-05 21:56:00 +1000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductListComponent } from './Products/product-list/product-list.component';
+import { ProductDetailComponent } from './Products/product-detail/product-detail.component';
 import { ConvertToSpacesPipe } from './common/convert-to-spaces.pipe';
 import { StarComponent } from './common/star/star.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
