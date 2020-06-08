6a62a0ccadbebb4ef41260e716ccd025bdc0a637
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-08 19:28:01 +1000

    update to product model

--- src/app/Products/product.ts
+++ src/app/Products/product.ts
@@ -3,6 +3,7 @@ export interface IProduct {
     productName: string;
     productCode: string;
     releaseDate: string;
+    tags?: string[];
     price: number;
     description: string;
     starRating: number;
